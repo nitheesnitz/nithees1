af61c123c4621e8873713ca108c74d2cb0c13b21
nitheessasti <43338385+nitheessasti@users.noreply.github.com>
2018-12-20 14:27:47 +0530

    Create program.c

---
+++ program.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int()
+{
+  int a;
+  printf("\nenter the number a:");
+  scanf("&d",&a);
+  if(a>0)
+  printf("%d is positive:",a);
+  if(a<0)
+  printf("%d is negative:",a);
+  if(a=0)
+  printf("%d is 0:",a);
+  return 0;
+}
+  
